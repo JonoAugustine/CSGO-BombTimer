14e2d6a28d2355ea4bd6d9f95d6c327ccb1b7a8f
JonoAugustine <jaugust8@jhu.edu>
2018-02-05 01:48:50 -0500

    Now sets vairables and then has a logic statement seperate

--- CSGO_BombAutoLaunch.bat
+++ CSGO_BombAutoLaunch.bat
@@ -1,26 +1,42 @@
-@ECHO OFF
-SET csrunning=false
-SET tmrunning=false
+@ECHO ON
+SET csrunning=0
+SET tmrunning=0
 SET CSGO=csgo.exe
 SET Timer=javaw.exe
 
 :A
 TIMEOUT /T 180
 
+REM is CSGO running? (1 = false)
 TASKLIST | FINDSTR /I %CSGO%
 IF ERRORLEVEL 1 ( 
-	SET csrunning=false
-    GOTO :T
+    SET csrunning=0
 ) ELSE ( 
-	SET csrunning=true
-    GOTO :T
+    SET csrunning=1
 )
 
-:T
+REM is Timer running? (1 = false)
 TASKLIST | FINDSTR /I %Timer%
 IF ERRORLEVEL 1 (
-	IF %csrunning% ( GOTO :Start ) 
-) ELSE ( TASKKILL /IM %Timer% /F /T )
+    SET tmrunning=0
+) ELSE ( 
+    SET tmrunning=1
+)
+
+REM IF cs is running and timer is not, 
+REM start the timer
+IF %csrunning%==1 (
+   IF %tmrunning%==1 ( 
+        echo ''
+   ) ELSE ( 
+        GOTO :Start 
+   )
+
+    REM If cs is not running and the timer is,
+    REM Stop the timer
+) ELSE (
+    IF %tmrunning%==1 TASKKILL /IM %Timer% /F /T
+)
 
 GOTO :A 
 
@@ -29,3 +45,25 @@ CALL "%~dp0\csgo.timer.jar"
 GOTO :A
 
 echo FAILED
+
+REM CSGO is running
+REM     Timer is running
+REM         
+REM     Do nothing
+REM
+REM
+REM CSGO is running
+REM     Timer is not running
+REM     
+REM     Start timer
+REM
+REM CSGO is not running
+REM     Timer is running
+REM
+REM     Stop timer
+REM
+REM CSGO is not running
+REM     Running is not running
+REM
+REM     Do nothing
+
